8506ff70adc7d0e1a5dd8513727fbd7d7867fa98
Kevin Robertson <dimanikolaev598@gmail.com>
2026-08-09 19:36:18 +0300

    chore: bump Android release to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "com.privatevpn.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
